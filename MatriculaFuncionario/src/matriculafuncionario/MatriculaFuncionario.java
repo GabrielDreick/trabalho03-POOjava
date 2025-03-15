214de66c09b12b6c19312ad9f22d286d4be72fe4
Gabriel Dreick <gabriel.dreick+github@gmail.com>
2025-03-15 16:07:36 -0300

    +pdf

--- MatriculaFuncionario/src/matriculafuncionario/MatriculaFuncionario.java
+++ MatriculaFuncionario/src/matriculafuncionario/MatriculaFuncionario.java
@@ -1,15 +1,11 @@
 
 package matriculafuncionario;
-
 /**
  *
  * @author Gabriel Dreick
  */
 public class MatriculaFuncionario {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Matricular fun1 = new Matricular();
         
