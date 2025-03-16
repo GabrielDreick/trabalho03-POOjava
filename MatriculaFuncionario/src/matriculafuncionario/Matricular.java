5219dfca7725420352fbbf3fef978494fae102ea
Gabriel Dreick <gabriel.dreick+github@gmail.com>
2025-03-16 09:54:09 -0300

    Update Matricular.java

--- MatriculaFuncionario/src/matriculafuncionario/Matricular.java
+++ MatriculaFuncionario/src/matriculafuncionario/Matricular.java
@@ -10,6 +10,8 @@ public class Matricular {
     
     private int matricula;
     private String nome;
+    private String dep1, dep2, dep3, dep4;
+    
     
     private Scanner ler = new Scanner(System.in);
     
@@ -36,12 +38,28 @@ public class Matricular {
         
         print("Name do Funcionario: \n>");
         this.nome = lerStr();
+        
+        print("Informes seus quatro dependentes. \n");
+        print("Dependente 1: ");
+        this.dep1 = lerStr();
+        
+        print("Dependente 2: ");
+        this.dep2 = lerStr();
+        
+        print("Dependente 3: ");
+        this.dep3 = lerStr();
+        
+        print("Dependente 4: ");
+        this.dep4 = lerStr();
+        
+        
     };
     
     
     public void info(){
       print(
       "Matricula: " + this.matricula +"\n"+
-      "Nome:      " + this.nome +"\n");  
+      "Nome:      " + this.nome +"\n"+
+      "Dependentes: \n" + (" "+this.dep1+" \n") + (" "+this.dep2+" \n") + (" "+this.dep3+" \n") + (" "+this.dep4+" \n"));
     };
 }
